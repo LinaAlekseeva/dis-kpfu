4c17db9ad26d3b264c7e8de56719efcb0b923fb4
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-07-12 12:13:56 +0300

    + commit

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,11 +3,9 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import config.WebDriverConfig;
-import config.WebDriverProvider;
+
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
